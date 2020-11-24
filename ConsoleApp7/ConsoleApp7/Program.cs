349a588707fb23725bed66b1315670483ffb3030
50lut10n <72496616+50lut10n@users.noreply.github.com>
2020-11-24 19:32:45 +0500

    Выполнено задание 5.2

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            var tros = "трос";
+            string sort = tros.Substring(3, 1) + tros.Substring(2, 1) + tros.Substring(1, 1) + tros.Substring(0, 1);
+            string rost = tros.Substring(1, 1) + tros.Substring(2, 1) + tros.Substring(3, 1) + tros.Substring(0, 1);
+            Console.WriteLine($"Результат: {sort}, {rost}.");
+            Console.ReadKey();
+        }
+    }
+}
