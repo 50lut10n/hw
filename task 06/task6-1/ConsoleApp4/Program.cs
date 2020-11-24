afec96600714bc5f7b814d1de2d67dd70980233d
50lut10n <72496616+50lut10n@users.noreply.github.com>
2020-11-24 15:32:27 +0500

    tasks 6.1; 6.2; 6.3 done

---
+++ task 06/task6-1/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            var n = Program.delenie("n");
+            
+            Console.WriteLine(n);
+        }
+
+        public static bool delenie (string x)
+        {
+            Console.WriteLine("vvedite chislo " + x);
+            var n = int.Parse(Console.ReadLine());
+            return n % 5 == 0 && n % 7 != 0;
+        }
+
+    }
+}
+
+
