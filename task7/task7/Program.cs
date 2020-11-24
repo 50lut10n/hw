9f78aa2731e4db9ea56eef4ed338775810bfe132
50lut10n <72496616+50lut10n@users.noreply.github.com>
2020-11-24 18:58:23 +0500

    Задание 7 выполнено

---
+++ task7/task7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedite x");
+            Console.WriteLine("f(x) = " + (MathFunction(double.Parse(Console.ReadLine()))));
+        }
+
+        public static double MathFunction (double a)
+        {
+            if (a>2)
+                return 2;
+            if (a > 0 && a <= 2)
+                return 0;
+            else
+                return -3 * a;
+        }
+    }
+}
