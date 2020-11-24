b7659078594b0d3a7ea143033baca7b82bdd6aa5
50lut10n <72496616+50lut10n@users.noreply.github.com>
2020-11-24 16:25:00 +0500

    Задание №3 выполнено

---
+++ task3/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathFunction("x"));
+        }
+
+        
+        public static double MathFunction(string a)
+        {
+            Console.WriteLine("vvedite chislo " + a);
+            double n = double.Parse(Console.ReadLine());
+            return Math.Sqrt((2 * n + Math.Sin(Math.Abs(3 * n))) / 3.56);
+        }
+    }
+}
