6d64238dd386851b9eae2bd9957f468cd3d352a9
Alexander Bogdanovsky <72496616+50lut10n@users.noreply.github.com>
2020-12-20 23:56:22 +0500

    Task 8 done

---
+++ task8-2/task8-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите количество элементов в ряду");
+            int amount = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Введите элементы ряда через запятую");
+            string initialstring = Console.ReadLine();
+
+            string[] allnumbers = initialstring.Split(',');
+
+            int resultat = 1;
+            for (int i = 0; i < amount; i++)
+            {
+                resultat *= int.Parse(allnumbers[i]);
+            }
+            Console.WriteLine($"\nРезульат: {resultat}");
+        }
+    }
+}
