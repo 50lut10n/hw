0cd0916d73b1b4ce411d7ae0a8ec0f8cb3d2631c
Alexander Bogdanovsky <72496616+50lut10n@users.noreply.github.com>
2020-12-21 00:00:06 +0500

    Task 8 done

---
+++ task8-6/task8-6/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace task8_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            
+            for (int i = 0; i < b - a + 1; i++) //Для каждого числа нужно проверить - не является ли оно простым или квадратом простого
+            {
+                int checknumber = a + i; //checknumber - поочередно каждое число между а и б включительно
+                if (Math.Sqrt(checknumber) == (double)(int)Math.Sqrt(checknumber)) //Проверка на то, что число checknumber квадрат целого числа
+                {
+                    if (IsNumberPrime((int)Math.Sqrt(checknumber))) //Проверка что корень checknumber не является простым числом
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.Write($"{checknumber} ");
+                    }
+                }
+                else // Число checknumber не является квадратом, нужно проверить - не является ли оно простым
+                {
+                    if (IsNumberPrime(checknumber))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.Write($"{checknumber} ");
+                    }
+                }
+            }
+
+    
+
+
+            static bool IsNumberPrime(int i) //Проверка на то, что число является простым
+            {
+                for (int j=2;j<= (int)Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        return false;
+                        break;
+                    }
+                }
+                return true;
+            }
+
+
+
+        }
+    }
+}
